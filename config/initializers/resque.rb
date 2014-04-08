94e57d12f2c4921fef100a28f9b0e72d471386a7
Juan Ruiz <invitado@perdidita.(none)>
2014-04-08 18:41:15 +0100

    Finished local resque

---
+++ config/initializers/resque.rb
@@ -0,0 +1,5 @@
+# config/initializers/resque.rb
+require 'resque'
+rails_env = ENV['RAILS_ENV'] || 'development'
+config = YAML.load_file(Rails.root.join 'config', 'resque.yml')
+Resque.redis = config[rails_env]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResqueApp::Application.config.secret_key_base = 'd2b546f04d48c3fa0c9c2e27de3c04c8ef67f9a9d9e84b9179342e8aa946d4331106d4c9cfc3f9428b437666eaf1e953b5fbd45630fce1f8a7720d3b1d3fe50c'
